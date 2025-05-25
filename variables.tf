d8532a199c9a64a45ccdf27844a3fb3bdac4ac8c
Sato, I <8ato2h1@gmail.com>
2025-05-25 00:28:45 +0000

    dev config

--- variables.tf
+++ variables.tf
@@ -15,3 +15,16 @@ variable "instance_name" {
   default     = "Provisioned by Terraform"
   type        = string
 }
+
+output "current_workspace_name" {
+  value = terraform.workspace
+}
+
+variable "HCP_TERRAFORM_RUN_ID" {
+  type    = string
+  default = ""
+}
+
+output "remote_execution_determine" {
+  value = "Remote run environment? %{if var.HCP_TERRAFORM_RUN_ID != ""}Yes%{else}No this is local%{endif}!"
+}
